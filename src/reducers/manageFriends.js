3821a197d7b94666fd7ef554e410f610b6cb2513
cdcardle <cdcardle@gmail.com>
2019-03-18 09:12:01 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND":
+      let newFriends = state.friends.filter(function(f){
+        if(f.id !== action.id){
+          return f
+        }
+      })
+      return {friends: newFriends}
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
 export function managePresents(state, action){
+  switch (action.type) {
+    case "INCREASE":
+      return {numberOfPresents: state.numberOfPresents + 1}
+    case "DECREASE":
+      return 
+    default: {numberOfPresents: state.numberOfPresents - 1}
+      return state;
+  }
 }
