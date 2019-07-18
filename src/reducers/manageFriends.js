73407eb2184fa461ba8eb2796bdc72a374784e24
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 16:34:53 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      let newFriends = state.friends;
+      newFriends.push(action.friend);
+      return {friends: newFriends};
+    case 'REMOVE_FRIEND':
+      return {friends: state.friends.filter(friend => friend.id !== action.id)};
+    default:
+      return state;
+  }
 }
